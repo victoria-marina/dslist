f84d0fb6f053faaa2a875834fbfe7b92c8197891
victoria-marina <victoria.marina@hotmail.com>
2025-05-22 01:25:16 -0300

    GET lists.

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -4,5 +4,4 @@ import com.devsuperior.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-
 }
